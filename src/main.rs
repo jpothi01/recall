5c58818ae79605b5de14524c5d0aebebef04b303
John Pothier <john.pothier7@gmail.com>
2020-10-10 14:39:18 -0400

    Almost listing notes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+use chrono;
 use serde_derive::Deserialize;
 use shellexpand;
 use sqlite::State;
+use std::convert::TryFrom;
 use std::env;
 use std::fs;
 use std::fs::File;
 use std::path::Display;
 use std::path::Path;
 use std::path::PathBuf;
+use std::time;
 use structopt::StructOpt;
 use toml::de;
 
@@ -18,11 +21,14 @@ struct Config {
 }
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "recall", about = "An example of StructOpt usage.")]
-struct Opt {
+#[structopt(
+    name = "recall",
+    about = "An integrated task tracking CLI application."
+)]
+struct Options {
     /// Input file
     #[structopt(parse(from_str))]
-    note_title: String,
+    note_title: Option<String>,
 }
 
 fn find_config_file() -> Option<Box<PathBuf>> {
@@ -39,23 +45,97 @@ fn find_config_file() -> Option<Box<PathBuf>> {
     return None;
 }
 
-fn run(config: Config) -> sqlite::Result<()> {
+struct Note {
+    datetime_millis: i64,
+    title: String,
+    path: Option<String>,
+    link: Option<String>,
+}
+
+impl Note {
+    fn new(title: String, path: Option<String>, link: Option<String>) -> Note {
+        Note {
+            datetime_millis: i64::try_from(
+                std::time::SystemTime::now()
+                    .duration_since(std::time::UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis(),
+            )
+            .unwrap(),
+            title: title,
+            path: path,
+            link: link,
+        }
+    }
+}
+
+fn insert_note(connnection: sqlite::Connection, note: Note) -> sqlite::Result<()> {
+    connnection.execute(format!(
+        "
+    INSERT INTO notes (datetime, title, link, path)
+    VALUES ({}, '{}', '{}', '{}')
+    ",
+        note.datetime_millis,
+        note.title,
+        note.path.unwrap_or(String::from("NULL")),
+        note.link.unwrap_or(String::from("NULL"))
+    ))
+}
+
+fn list_notes(connection: sqlite::Connection) -> sqlite::Result<Vec<Note>> {
+    let mut statement = connection
+        .prepare("SELECT (datetime, title, link, path) FROM notes WHERE archived = FALSE")?;
+
+    let mut result = Vec::<Note>::new();
+    while let State::Row = statement.next()? {
+        let datetime = statement.read::<i64>(0)?;
+        let title = statement.read::<String>(1)?;
+        let link = statement.read::<Option<String>>(2)?;
+        let path = statement.read::<Option<String>>(3)?;
+        result.push(Note {
+            datetime_millis: datetime,
+            title: title,
+            link: link,
+            path: path,
+        });
+    }
+
+    Ok(result)
+}
+
+fn run(config: Config, options: Options) -> sqlite::Result<()> {
     let connection = sqlite::open(Path::new(&*shellexpand::tilde(&config.db_path)))?;
 
-    connection
-        .execute(
-            "
-        CREATE TABLE users (name TEXT, age INTEGER);
-        INSERT INTO users (name, age) VALUES ('Alice', 42);
-        INSERT INTO users (name, age) VALUES ('Bob', 69);
+    connection.execute(
+        "
+        CREATE TABLE IF NOT EXISTS notes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            datetime INTEGER NOT NULL,
+            archived BOOLEAN NOT NULL DEFAULT FALSE,
+            title TEXT NOT NULL,
+            link TEXT,
+            path TEXT
+        );
         ",
-        )
-        .unwrap();
+    )?;
 
-    Ok(())
+    match options.note_title {
+        Some(note_title) => insert_note(connection, Note::new(note_title, None, None)),
+        None => {
+            for note in list_notes(connection)? {
+                println!(
+                    "{}\t|{}",
+                    chrono::NaiveDateTime::from_timestamp(note.datetime_millis, 0),
+                    note.title
+                );
+            }
+            Ok(())
+        }
+    }
 }
 
 fn main() {
+    let options = Options::from_args();
     let maybe_config_file = find_config_file();
     match maybe_config_file {
         None => println!("Could not find config file. Create a file named '{}' in an ancestor to the current directory", CONFIG_FILENAME),
@@ -64,7 +144,7 @@ fn main() {
                 Ok(config_string) => {
                     let maybe_config = toml::from_str::<Config>(&config_string);
                     match maybe_config {
-                        Ok(config) => match run(config) {
+                        Ok(config) => match run(config, options) {
                             Ok(()) => (),
                             Err(err) => println!("Error: {}", err)
                         },
@@ -75,15 +155,4 @@ fn main() {
             }
         }
     }
-
-    // let mut statement = connection
-    //     .prepare("SELECT * FROM users WHERE age > ?")
-    //     .unwrap();
-
-    // statement.bind(1, 50).unwrap();
-
-    // while let State::Row = statement.next().unwrap() {
-    //     println!("name = {}", statement.read::<String>(0).unwrap());
-    //     println!("age = {}", statement.read::<i64>(1).unwrap());
-    // }
 }
