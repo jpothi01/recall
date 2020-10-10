698db3a1dfc2e06ace68201fc7e14dd1e81084a6
John Pothier <john.pothier7@gmail.com>
2020-10-10 16:59:30 -0400

    Display, refactor, etc

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,15 @@ struct Options {
 
     #[structopt(short = "a", long = "archive")]
     archive: bool,
+
+    #[structopt(short = "p", long = "path")]
+    path: Option<String>,
+
+    #[structopt(short = "l", long = "link")]
+    link: Option<String>,
+
+    #[structopt(short = "t", long = "text")]
+    text: Option<String>,
 }
 
 fn find_config_file() -> Option<Box<PathBuf>> {
@@ -49,15 +58,20 @@ fn find_config_file() -> Option<Box<PathBuf>> {
     return None;
 }
 
+enum NoteContent {
+    Path(String),
+    Link(String),
+    Text(String),
+}
+
 struct Note {
     datetime_millis: i64,
     title: String,
-    path: Option<String>,
-    link: Option<String>,
+    content: Option<NoteContent>,
 }
 
 impl Note {
-    fn new(title: String, path: Option<String>, link: Option<String>) -> Note {
+    fn new(title: String) -> Note {
         Note {
             datetime_millis: i64::try_from(
                 std::time::SystemTime::now()
@@ -67,45 +81,163 @@ impl Note {
             )
             .unwrap(),
             title: title,
-            path: path,
-            link: link,
+            content: None,
+        }
+    }
+
+    fn new_with_path(title: String, path: String) -> Note {
+        Note {
+            datetime_millis: i64::try_from(
+                std::time::SystemTime::now()
+                    .duration_since(std::time::UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis(),
+            )
+            .unwrap(),
+            title: title,
+            content: Some(NoteContent::Path(path)),
+        }
+    }
+
+    fn new_with_link(title: String, link: String) -> Note {
+        Note {
+            datetime_millis: i64::try_from(
+                std::time::SystemTime::now()
+                    .duration_since(std::time::UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis(),
+            )
+            .unwrap(),
+            title: title,
+            content: Some(NoteContent::Link(link)),
+        }
+    }
+
+    fn new_with_text(title: String, text: String) -> Note {
+        Note {
+            datetime_millis: i64::try_from(
+                std::time::SystemTime::now()
+                    .duration_since(std::time::UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis(),
+            )
+            .unwrap(),
+            title: title,
+            content: Some(NoteContent::Text(text)),
         }
     }
 }
 
 fn insert_note(connnection: sqlite::Connection, note: Note) -> sqlite::Result<()> {
+    let (path, link, text) = match note.content {
+        None => (None, None, None),
+        Some(content) => match content {
+            NoteContent::Path(path) => (Some(format!("'{}'", path)), None, None),
+            NoteContent::Link(link) => (None, Some(format!("'{}'", link)), None),
+            NoteContent::Text(text) => (None, None, Some(format!("'{}'", text))),
+        },
+    };
+
     connnection.execute(format!(
         "
-    INSERT INTO notes (datetime, title, link, path)
-    VALUES ({}, '{}', {}, {})
+    INSERT INTO notes (datetime, title, path, link, text)
+    VALUES ({}, '{}', {}, {}, {})
     ",
         note.datetime_millis,
         note.title,
-        format!("'{}'", note.path.unwrap_or(String::from("NULL"))),
-        format!("'{}'", note.link.unwrap_or(String::from("NULL")))
+        path.unwrap_or(String::from("NULL")),
+        link.unwrap_or(String::from("NULL")),
+        text.unwrap_or(String::from("NULL"))
     ))
+}
+
+fn read_note(statement: &mut sqlite::Statement) -> sqlite::Result<Note> {
+    let datetime = statement.read::<i64>(0)?;
+    let title = statement.read::<String>(1)?;
+    let path = statement.read::<Option<String>>(2)?;
+    let link = statement.read::<Option<String>>(3)?;
+    let text = statement.read::<Option<String>>(4)?;
+
+    let note = if let Some(path) = path {
+        Note {
+            datetime_millis: datetime,
+            title: title,
+            content: Some(NoteContent::Path(path)),
+        }
+    } else if let Some(link) = link {
+        Note {
+            datetime_millis: datetime,
+            title: title,
+            content: Some(NoteContent::Link(link)),
+        }
+    } else if let Some(text) = text {
+        Note {
+            datetime_millis: datetime,
+            title: title,
+            content: Some(NoteContent::Text(text)),
+        }
+    } else {
+        Note {
+            datetime_millis: datetime,
+            title: title,
+            content: None,
+        }
+    };
+    Ok(note)
 }
 
 fn list_notes(connection: sqlite::Connection) -> sqlite::Result<Vec<Note>> {
     let mut statement = connection.prepare(
-        "SELECT datetime, title, link, path FROM notes WHERE archived = FALSE ORDER BY datetime",
+        "SELECT datetime, title, path, link, text FROM notes WHERE archived = FALSE ORDER BY datetime",
     )?;
 
     let mut result = Vec::<Note>::new();
     while let State::Row = statement.next()? {
-        let datetime = statement.read::<i64>(0)?;
-        let title = statement.read::<String>(1)?;
-        let link = statement.read::<Option<String>>(2)?;
-        let path = statement.read::<Option<String>>(3)?;
-        result.push(Note {
-            datetime_millis: datetime,
-            title: title,
-            link: link,
-            path: path,
-        });
+        result.push(read_note(&mut statement)?);
     }
 
     Ok(result)
+}
+
+fn note_display_string(note: &Note) -> String {
+    let content_display = match &note.content {
+        Some(NoteContent::Path(path)) => Some(path.bold()),
+        Some(NoteContent::Link(link)) => Some(link.cyan()),
+        Some(NoteContent::Text(text)) => Some(text.italic()),
+        None => None,
+    };
+    let title_display = note.title.yellow();
+    let time_display = chrono::DateTime::<chrono::Local>::from(
+        std::time::UNIX_EPOCH
+            + std::time::Duration::from_millis(u64::try_from(note.datetime_millis).unwrap()),
+    );
+    match content_display {
+        None => format!("{}\t{}", time_display, title_display),
+        Some(content_display) => {
+            format!("{}\t{}\t{}", time_display, title_display, content_display)
+        }
+    }
+}
+
+fn display_note(connection: sqlite::Connection, note_index: i64) -> sqlite::Result<()> {
+    // TODO: de-dupe
+    let mut statement = connection.prepare(
+        "SELECT id, title, path, link, text FROM notes WHERE archived = FALSE ORDER BY datetime",
+    )?;
+
+    let mut current_index = 0;
+    while let State::Row = statement.next()? {
+        if current_index != note_index {
+            current_index += 1;
+            continue;
+        }
+
+        println!("{}", note_display_string(&read_note(&mut statement)?));
+        return Ok(());
+    }
+
+    println!("Note not found.");
+    return Ok(());
 }
 
 fn archive_note(connection: sqlite::Connection, note_index: i64) -> sqlite::Result<()> {
@@ -148,8 +280,9 @@ fn run(config: Config, options: Options) -> sqlite::Result<()> {
             datetime INTEGER NOT NULL,
             archived BOOLEAN NOT NULL DEFAULT FALSE,
             title TEXT NOT NULL,
+            path TEXT,
             link TEXT,
-            path TEXT
+            text TEXT
         );
         ",
     )?;
@@ -174,19 +307,28 @@ fn run(config: Config, options: Options) -> sqlite::Result<()> {
     }
 
     match options.note_title_or_index {
-        Some(note_title) => insert_note(connection, Note::new(note_title, None, None)),
+        Some(note_title_or_index) => {
+            let note_index = note_title_or_index.parse::<i64>();
+            match note_index {
+                Ok(note_index) => display_note(connection, note_index),
+                Err(err) => {
+                    let note_title = note_title_or_index;
+                    // TODO: validate mutually exclusive
+                    if let Some(path) = options.path {
+                        insert_note(connection, Note::new_with_path(note_title, path))
+                    } else if let Some(link) = options.link {
+                        insert_note(connection, Note::new_with_link(note_title, link))
+                    } else if let Some(text) = options.text {
+                        insert_note(connection, Note::new_with_text(note_title, text))
+                    } else {
+                        insert_note(connection, Note::new(note_title))
+                    }
+                }
+            }
+        }
         None => {
-            for note in list_notes(connection)? {
-                println!(
-                    "{}\t{}",
-                    chrono::DateTime::<chrono::Local>::from(
-                        std::time::UNIX_EPOCH
-                            + std::time::Duration::from_millis(
-                                u64::try_from(note.datetime_millis).unwrap()
-                            )
-                    ),
-                    note.title.yellow()
-                );
+            for (i, note) in list_notes(connection)?.iter().enumerate() {
+                println!("{} {}", format!("{}", i).bold(), note_display_string(note));
             }
             Ok(())
         }
